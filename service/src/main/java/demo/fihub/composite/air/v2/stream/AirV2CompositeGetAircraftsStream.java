13e84b4b3e85a4b3ca9dac37912b6218e748c620
Samriddha Chatterjee <samriddhac@gmail.com>
2021-01-27 17:35:15 +0530

    air-v2

---
+++ service/src/main/java/demo/fihub/composite/air/v2/stream/AirV2CompositeGetAircraftsStream.java
@@ -0,0 +1,48 @@
+package demo.fihub.composite.air.v2.stream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.stereotype.Component;
+
+import com.google.pubsub.v1.PubsubMessage;
+import com.macys.order.foundation.core.utils.execution.ServiceContextUtil;
+import com.macys.order.foundation.core.utils.json.JsonUtils;
+import com.macys.order.foundation.core.utils.logging.Logger;
+import com.macys.order.foundation.core.utils.message.validation.MessageValidator;
+import com.macys.order.foundation.core.utils.msg.subsciber.AbstractStreamSubscriber;
+import demo.fihub.composite.air.v2.IAirV2CompositeService;
+
+import demo.fihub.composite.air.v2.model.*;
+import brave.Tracer;
+
+@Component
+public class AirV2CompositeGetAircraftsStream extends AbstractStreamSubscriber
+    implements ServiceContextUtil {
+
+  @Value("${topic.subscription.getaircrafts.name}")
+  private String subscriptionName;
+
+  private final JsonUtils jsonUtils;
+
+  private final IAirV2CompositeService airV2CompositeService;
+
+  public AirV2CompositeGetAircraftsStream(
+      PubSubTemplate pubSubTemplate,
+      MessageValidator messageValidator,
+      JsonUtils jsonUtils,
+      Logger logger,
+      Tracer tracer,
+      IAirV2CompositeService airV2CompositeService) {
+    super(pubSubTemplate, messageValidator, logger, tracer);
+    this.airV2CompositeService = airV2CompositeService;
+    this.jsonUtils = jsonUtils;
+  }
+
+  @Override
+  protected void invokeService(PubsubMessage message) {}
+
+  @Override
+  protected String getTopicSubscription() {
+    return subscriptionName;
+  }
+}
--- service/src/main/java/demo/fihub/core/order/id/v7/model/Order.java
+++ service/src/main/java/demo/fihub/core/order/id/v7/model/Order.java
@@ -21,7 +21,7 @@ import javax.validation.constraints.*;
  * 
  */
 @ApiModel(description = "")
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2021-01-27T17:15:12.496+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2021-01-27T17:35:13.494+05:30")
 public class Order   {
 
   @Override
