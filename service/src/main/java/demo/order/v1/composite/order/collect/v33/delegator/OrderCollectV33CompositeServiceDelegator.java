5b85e3849ad4fc9ef40c99a9826182b9f7f2cc31
Samriddha Chatterjee <samriddhac@gmail.com>
2021-02-12 10:57:13 +0530

    order-collect-v33

--- service/src/main/java/demo/order/v1/composite/order/collect/v33/delegator/OrderCollectV33CompositeServiceDelegator.java
+++ service/src/main/java/demo/order/v1/composite/order/collect/v33/delegator/OrderCollectV33CompositeServiceDelegator.java
@@ -1,5 +1,6 @@
 package demo.order.v1.composite.order.collect.v33.delegator;
 
+import com.abcd.uop.model.Order;
 import com.macys.mst.order.foundation.core.utils.execution.ServiceExecutionContextUtil;
 import com.macys.mst.order.foundation.core.utils.future.FutureUtils;
 import demo.order.v1.composite.order.collect.v33.IOrderCollectV33CompositeService;
@@ -20,20 +21,20 @@ public class OrderCollectV33CompositeServiceDelegator
   private final IShipmentV4CoreServiceHelper shipmentV4CoreServiceHelper;
 
   @Override
-  public void collectOrder() {
+  public Order collectOrder(Order body) {
     orderCollectV33CompositeServiceHelper.processCollectOrderRequest();
     if (isServiceCallEnabled("6bd44e49-51b1-44b6-bd11-484db148105e")) {
       shipmentV4CoreServiceHelper.createShipment("6bd44e49-51b1-44b6-bd11-484db148105e");
     }
-    orderCollectV33CompositeServiceHelper.processCollectOrderResponse();
+    return orderCollectV33CompositeServiceHelper.processCollectOrderResponse();
   }
 
   @Override
-  public void updateOrder() {
+  public com.abcd.oop.model.Order updateOrder(com.abcd.oop.model.Order body) {
     orderCollectV33CompositeServiceHelper.processUpdateOrderRequest();
     if (isServiceCallEnabled("f612fbaf-f3d5-41d6-8a66-3dd7183ea1b1")) {
       shipmentV4CoreServiceHelper.updateShipment("f612fbaf-f3d5-41d6-8a66-3dd7183ea1b1");
     }
-    orderCollectV33CompositeServiceHelper.processUpdateOrderResponse();
+    return orderCollectV33CompositeServiceHelper.processUpdateOrderResponse();
   }
 }
